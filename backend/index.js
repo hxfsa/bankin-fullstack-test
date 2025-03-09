ccbd56a6a440ad1c7ee9e464a0824cd9121ac0b9
Hafsa <hafsa.aitouarraou@outlook.fr>
2025-03-09 15:12:55 +0100

    getting all accounts

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const axios = require("axios");
 require("dotenv").config();
 
+
+const accessToken = process.env.ACCESSTOKEN;
+
+//Etape 1: Authentification d'un utilisateur
 const authenticate = async () => {
   const url = "https://sync.bankin.com/v2/authenticate";
   const data = {
@@ -22,3 +26,23 @@ const authenticate = async () => {
 };
 
 authenticate();
+
+//Etape 2 : récupération des comptes
+const getAccounts = async (accessToken) => {
+  const url = "https://sync.bankin.com/v2/accounts";
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "bankin-version": "2019-08-22",
+    "bankin-device": "26ac2fb6-4b1f-4e7c-a35d-aaa40b5c00b5",
+  };
+
+  try {
+    const response = await axios.get(url, { headers });
+    console.log(response.data.resources)
+    return response.data.resources ?? [];
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getAccounts(accessToken);
